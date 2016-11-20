045a5defb316eba383ca8c8b68aa2f14e6ab8d19
Titus Wormer <tituswormer@gmail.com>
2016-11-20 11:05:06 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,57 +1,19 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module unist:util:generated
- * @fileoverview Check if a node is generated.
- */
-
 'use strict';
 
-/* eslint-env commonjs */
-
-/**
- * Return `value` if it’s an object, an empty object
- * otherwise.
- *
- * @private
- * @param {*} value - Value to check.
- * @return {Object} - `value` or `{}`.
- */
-function optional(value) {
-    return value && typeof value === 'object' ? value : {};
-}
-
-/**
- * Detect if a node was available in the original document.
- *
- * @example
- *   generated(); // true
- *
- *   generated({
- *     start: {
- *       line: 1,
- *       column: 1
- *     },
- *     end: {
- *       line: 1,
- *       column: 2
- *     }
- *   }); // false
- *
- * @param {Node} node - Node to check.
- * @return {boolean} - Whether or not `node` is generated.
- */
-function generated(node) {
-    var position = optional(optional(node).position);
-    var start = optional(position.start);
-    var end = optional(position.end);
-
-    return !start.line || !start.column || !end.line || !end.column;
-}
-
-/*
- * Expose.
- */
-
+/* Expose. */
 module.exports = generated;
+
+/* Detect if a node was available in the original document. */
+function generated(node) {
+  var position = optional(optional(node).position);
+  var start = optional(position.start);
+  var end = optional(position.end);
+
+  return !start.line || !start.column || !end.line || !end.column;
+}
+
+/* Return `value` if it’s an object, an empty object
+ * otherwise. */
+function optional(value) {
+  return value && typeof value === 'object' ? value : {};
+}
--- test.js
+++ test.js
@@ -1,78 +1,55 @@
-/**
- * @author Titus Wormer
- * @copyright 2016 Titus Wormer
- * @license MIT
- * @module unist:util:generated
- * @fileoverview Test suite for `unist-util-generated`.
- */
-
 'use strict';
-
-/* eslint-env node */
-
-/*
- * Dependencies.
- */
 
 var test = require('tape');
 var generated = require('./index.js');
 
-/*
- * Tests.
- */
-
 test('generated', function (t) {
-    t.equal(
-        generated(),
-        true,
-        'should not throw without node'
-    );
+  t.equal(
+    generated(),
+    true,
+    'should not throw without node'
+  );
 
-    t.equal(
-        generated({
-            'position': {
-                'start': {
-                    'line': 1,
-                    'column': 1,
-                    'indent': [],
-                    'offset': 0
-                },
-                'end': {
-                    'line': 1,
-                    'column': 2,
-                    'indent': [],
-                    'offset': 1
-                }
-            }
-        }),
-        false,
-        'should return false when with properties'
-    );
+  t.equal(
+    generated({
+      position: {
+        start: {
+          line: 1,
+          column: 1,
+          indent: [],
+          offset: 0
+        },
+        end: {
+          line: 1,
+          column: 2,
+          indent: [],
+          offset: 1
+        }
+      }
+    }),
+    false,
+    'should return false when with properties'
+  );
 
-    t.equal(
-        generated({
-            'position': {
-                'start': {},
-                'end': {}
-            }
-        }),
-        true,
-        'should return true when without properties'
-    );
+  t.equal(
+    generated({
+      position: {start: {}, end: {}}
+    }),
+    true,
+    'should return true when without properties'
+  );
 
-    t.equal(
-        generated({
-            'position': {}
-        }),
-        true,
-        'should return true when without objects'
-    );
+  t.equal(
+    generated({position: {}}),
+    true,
+    'should return true when without objects'
+  );
 
-    t.equal(
-        generated({}),
-        true,
-        'should return true when without position'
-    );
+  t.equal(
+    generated({}),
+    true,
+    'should return true when without position'
+  );
 
-    t.end();
+  t.end();
 });
